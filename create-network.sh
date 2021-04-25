7c8c9f367ab210ee4e7c32b49b59d666311bf63e
santoadji21 <ahmad.aji.santoso21@gmail.com>
2021-04-25 18:24:11 +0700

    add config

--- create-network.sh
+++ create-network.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker network create -d overlay traefik-proxy
+echo -n "Deploy Swarm y/n: "
+read input
+
+if [ $input = "y" ];
+then
+  docker network create -d overlay traefik-proxy
+  echo "Create Traefik Network For Docker Swarm"
+else
+  docker network create traefik-network
+  echo "Create Traefik Network For Docker Compose"
+fi
