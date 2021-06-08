7b0d4ad3dfa1e58b79aee554e82e548062e2dbad
santoadji21 <ahmad.aji.santoso21@gmail.com>
2021-06-08 18:06:59 +0700

    add docker-compose

--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,30 @@
 <?php
- 
+require_once('config.php');
+
+
+// $sql="show variables where Variable_name like 'hostname' ";
+// $servername = "localhost";
+// $username = "moodle";
+// $password = "moodle_db";
+// $dbname = "moodle";
+
+// Create connection
+$conn = mysqli_connect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass, $CFG->dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+ $sql = "show variables where Variable_name like 'hostname' ";
+// $result = mysqli_query($conn, $sql);
+
+$res = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_assoc($res)) {
+  echo 'Database '.$row['Variable_name'].':'.$row['Value']."<br>\n";
+}
+
 phpinfo();
- 
+
 ?>
+
+<!-- mysql -u bn_moodle -h moodle -pbitnami_moodle -->
